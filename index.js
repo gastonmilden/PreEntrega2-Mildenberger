847b468687f203438ba2b07411a3a22a1475839f
Gaston Mildenberger <gastonmilden@gmail.com>
2023-09-18 17:41:17 -0300

    PreEntrega2+Mildenberger

--- index.js
+++ index.js
@@ -1,41 +1,78 @@
-
-//FUNCTION:
-
-function calcularPromedio() {
-    const cantidadDePartidos = parseInt (
-        prompt("Ingrese el total de partidos jugados hasta la fecha:")
-    );
-    console.log("Cantidad de partidos jugados de tu equipo: " + cantidadDePartidos);
-
-    let promedio = 0;
-    let totalPuntos = 0;
-
-    alert("Te recordamos los puntos obtenidos de tu equipo en función del resultado. \nPartido ganado son 3 puntos. \nPartido empatado es 1 punto. \nPartido perdido son 0 puntos.");
-
-    for (let index = 0; index < cantidadDePartidos; index++) {
-        const puntosObtenidos = parseFloat(
-            prompt("Ingrese la cantidad de puntos obtenidos en la fecha N°: " + (index + 1))
-        );
-        
-        if (puntosObtenidos === 3 || puntosObtenidos === 1 || puntosObtenidos === 0) {
-            totalPuntos += puntosObtenidos;
-            console.log("Cantidad de puntos obtenidos de tu equipo en el torneo: " + totalPuntos);
-            promedio = totalPuntos / cantidadDePartidos;
-        } else {
-            alert("La cantidad de puntos es inválida. Intente nuevamente.");
-            index--;
-        }
+const teams = [
+    {
+        id: 1,
+        name: "Boca",
+        gamesPlayed: 5,
+        points: 12
+    },
+    {
+        id: 2,
+        name: "Velez",
+        gamesPlayed: 5,
+        points: 15
+    },
+    {
+        id: 3,
+        name: "River",
+        gamesPlayed: 5,
+        points: 10
     }
-    alert("El promedio de puntos de tu equipo es: " + promedio.toFixed(3));
+];
 
-    if (promedio >= 1) {
-        alert("Es probable que tu equipo se quede en primera división.")
-    } else {
-        alert("Tu equipo bajará de categoría con este promedio.")
+function mostrarEquipos(teams) {
+    let listaDeEquipos = "";
+
+    for (const team of teams) {
+        listaDeEquipos += `
+        Equipo: ${team.name}
+        Partidos Jugados: ${team.gamesPlayed}
+        Puntos: ${team.points}\n`
     }
 
-    console.log('Promedio del equipo: ' + promedio.toFixed(3));
+    return listaDeEquipos;
 }
 
-calcularPromedio();
+alert(mostrarEquipos(teams));
 
+// Agregar y eliminar equipos hasta que el usuario decida finalizar
+while (true) {
+    const accion = prompt("¿Desea agregar un nuevo equipo o eliminar uno existente? (agregar/eliminar/finalizar)").toLowerCase();
+
+    if (accion === "finalizar") {
+        break; // Terminar de agregar equipos
+    } else if (accion === "agregar") {
+        const nuevoEquipo = {
+            name: prompt("Nuevo equipo:"),
+            gamesPlayed: parseInt(prompt("Cantidad de partidos jugados:")),
+            points: parseInt(prompt("Cantindad de puntos obtenidos:"))
+        };
+
+        teams.push(nuevoEquipo);
+
+        // Ordenar los equipos por cantidad de puntos obtenidos
+        teams.sort((a, b) => b.points - a.points);
+
+        // Mostrar la lista actualizada de equipos
+        alert(mostrarEquipos(teams));
+    } else if (accion === "eliminar") {
+        const nombreEquipoAEliminar = prompt("Nombre del equipo que queres eliminar:");
+
+        const index = teams.findIndex(team => team.name === nombreEquipoAEliminar);
+
+        if (index !== -1) {
+            teams.splice(index, 1);
+            alert(`El equipo "${nombreEquipoAEliminar}" ha sido eliminado.`);
+        } else {
+            alert(`No se encontró un equipo con el nombre "${nombreEquipoAEliminar}".`);
+        }
+    } else {
+        alert("Por favor, completar con 'agregar', 'eliminar' o 'finalizar'.");
+    }
+}
+
+// Mostrar la lista final de equipos
+alert("Lista final de equipos:\n" + mostrarEquipos(teams));
+
+
+//Mostrar en consola las posiciones una vez finalizada las funciones
+console.log("Lista final de equipos:\n" + mostrarEquipos(teams));
